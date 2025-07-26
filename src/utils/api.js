9291bffe2398cfe6403bdcdcfc54c74c4595761d
hamzaMallem <mallemdevs@gmail.com>
2025-07-26 07:50:43 +0100

    حل التعارضات

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export async function fetchPrayerTimes(city) {
-  const res = await axios.get("http://api.aladhan.com/v1/timingsByCity", {
+  const res = await axios.get("https://api.aladhan.com/v1/timingsByCity", {
     params: { city, country: "Morocco", method: 5 },
   });
   return res.data.data;
